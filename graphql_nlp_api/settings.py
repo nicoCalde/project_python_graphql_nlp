d490a39f354eb21fae8fcfece29e2e65bf7f25b8
nicoCalde <nicogonzalezcalderon@gmail.com>
2024-07-29 18:08:45 -0300

    REST API pagination added

--- graphql_nlp_api/settings.py
+++ graphql_nlp_api/settings.py
@@ -130,3 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 GRAPHENE = {
     "SCHEMA": "django_root.schema.schema"
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10  # Default number of items per page
+}
