185550c882e39c3fd90177711f3cfb98e7935fdb
nicoCalde <nicogonzalezcalderon@gmail.com>
2024-11-25 20:39:50 -0300

    Set up .env file with environment variables and integrated OpenAI API key for NLP endpoint development.

--- api_service/views.py
+++ api_service/views.py
@@ -4,9 +4,18 @@ from rest_framework import status
 from django.db.models import Q
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from openai import OpenAI
 from rest_framework import generics
 from .models import Cars
 from .serializers import CarsSerializer
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key= os.getenv('OPENAI_API_KEY'),
+)
 
 # Create your views here.
 class CarsList(generics.ListCreateAPIView):
--- graphql_nlp_api/settings.py
+++ graphql_nlp_api/settings.py
@@ -11,21 +11,25 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xx@%f%#k6j)phwmxx8u$8=@%t7=x&w@nj8phh*6-d-!%)_i0$%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
